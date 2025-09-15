c6fc2987644588d68c5a3b9c8688523342266ea5
RG <rng31883@hawkmail.hacc.edu>
2025-09-15 16:49:11 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.WriteLine("What is your name?");
         string name = Console.ReadLine();
 
-        Console.WriteLine($"Hi, {name}! Nice to meet you.");
+        Console.WriteLine($"Hi, {name}! Nice to meet you. It's great to be in the same class!");
 
         Console.WriteLine("What is your favorite hobby?");
         string hobby = Console.ReadLine().ToLower();
@@ -35,4 +35,5 @@ class Program
             Console.WriteLine($"Did you know that your name has {name.Length} letters?");
         }
     }
+
 }
